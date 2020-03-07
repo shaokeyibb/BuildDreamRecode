6d86896251ff2a84a36bbce126bcf0facfda8d02
shaokeyibb <shaokeyibb@126.com>
2020-03-07 13:58:01 +0800

    修梦境列表刷新逻辑

--- src/main/java/cn/lingyuncraft/builddreamrecode/command/subcommand/ReleaseCmd.java
+++ src/main/java/cn/lingyuncraft/builddreamrecode/command/subcommand/ReleaseCmd.java
@@ -28,6 +28,12 @@ public class ReleaseCmd implements PCommand {
                     plugin.lang.send(user, plugin.lang.build(plugin.localeKey, LocaleUtil.Type.WARN, "目标公众ID无效."));
                 } else {
                     Dream dream = Storage.get().get(args[1]);
+                    if (dream==null){
+                        plugin.lang.logError("筑梦", args[1], "Dream 对象为 null.");
+                        plugin.lang.log("正尝试重新初始化以加载梦境......",LocaleUtil.Type.ERROR,false);
+                        Storage.get().initWithSilent();
+                        dream = Storage.get().get(args[1]);
+                    }
                     if (user.getUniqueId()==dream.author){
                         DreamControl.releaseDream(args[1], user.getUniqueId());
                     }else{
